2ae2f7e15ae7ba3ed7be60031e5ad9b96ec9619f
daokimluc <daokimluc9xx@gmail.com>
2018-12-07 16:01:18 +0700

    17/07

---
+++ Shop.Model/Abstract/Auditable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string CreatedBy { set; get; }
+
+        public DateTime? UpdateDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdatedBy { set; get; }
+
+        [MaxLength(256)]
+        public string MetaKeyWord { set; get; }
+
+        [MaxLength(256)]
+        public string MetaDescription { set; get; }
+
+        public bool Status { set; get; }
+    }
+}
